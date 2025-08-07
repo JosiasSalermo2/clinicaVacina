4589d1e9845fe919f474362477f8e83a37ebcf23
Annefrance Costa <annefrance.costa@gmail.com>
2025-08-06 22:34:32 -0300

    App - adicao Login

--- sgcv-app/src/App.jsx
+++ sgcv-app/src/App.jsx
@@ -3,18 +3,21 @@ import 'bootswatch/dist/flatly/bootstrap.css';
 import 'toastr/build/toastr.min';
 import 'toastr/build/toastr.css';
 
+import { useLocation } from 'react-router-dom';
+
 import NavBar from './components/NavBar.jsx';
 import Rotas from './rotas.jsx';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='container'>
-        <Rotas />
-        <NavBar />
-      </div>
-    );
-  }
+function App() {
+  const location = useLocation();
+  const esconderNavBar = location.pathname === '/Login';
+
+  return (
+    <div className='container'>
+      {!esconderNavBar && <NavBar />}
+      <Rotas />
+    </div>
+  );
 }
 
 export default App;
